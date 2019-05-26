b706f4a1d65f66c035bd6a43b0ada298b44f4fa6
rafif favian <rafiffavian584@gmail.com>
2019-05-26 22:37:12 +0700

    layouts

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'FrontController@index')->name('front');
+Auth::routes();
+
